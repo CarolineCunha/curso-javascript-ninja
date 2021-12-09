842c5c43f14558a454a070dc95a27f2d3418dd40
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-12-09 06:48:15 -0300

    challenge-02

---
+++ challenge-02/index.js
@@ -0,0 +1,20 @@
+function soma(num1, num2){
+    return num1+num2
+}
+var soma = soma(1,3)
+console.log(soma)
+
+soma_nova = (num_1,num_2) => {
+ return num_1+num_2
+}
+var soma_nova = soma_nova(5,4)
+console.log(soma_nova)
+
+var sem_valor
+
+function add_valor() {
+    sem_valor = 10
+    return `o valor da variável agora é ${sem_valor}`
+}
+var add_valor = add_valor()
+console.log(add_valor)
