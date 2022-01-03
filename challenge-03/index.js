4cd5788142f82baf6cacda1d3ef32844db727bdb
unknown <caroline.cunha.b@hotmail.com>
2022-01-03 08:06:19 -0300

    challenge-03

---
+++ challenge-03/index.js
@@ -0,0 +1,12 @@
+var objetoVazio = {}
+var pessoa = {
+    nome: 'Carol',
+    sobrenome: 'Cunha',
+    sexo: 'Feminino',
+    idade: 28,
+    altura: 1.58,
+    peso: 48,
+    andando: false,
+    caminhouQuantosMetros: 0
+}
+
