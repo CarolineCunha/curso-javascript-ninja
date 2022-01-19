f2e71f4087dbcbceeed08ac0327627022c23b7fd
unknown <caroline.cunha.b@hotmail.com>
2022-01-19 08:05:55 -0300

    resolução

--- challenge-05/index.js
+++ challenge-05/index.js
@@ -9,3 +9,26 @@ var teste = recebeArray[1]
 function recebeArrayNum(arr, num){
     return arr[num]
 }
+
+function book(bookName) {
+    var allBooks = {
+        'Segredos do Ninja Javascript': {
+            quantidadePaginas: 488,
+            autor: 'John Resig & Bear Bibeault',
+            editora: 'Novatec'
+        },
+        'Introdução ao HTML5': {
+            quantidadePaginas: 220,
+            autor: 'Bruce  Lawson & Remy Sharp',
+            editora: 'Alta Books'
+        },
+        'Smashing CSS': {
+            quantidadePaginas: 283,
+            autor: 'Erick A. Meyer',
+            editora: 'Bookman'
+
+        },
+    }
+
+    return !bookName ? allBooks : allBooks[bookName]
+}
