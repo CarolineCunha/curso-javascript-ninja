1fadd8c02e60274ef851f8d8acc6698c53c6fb7a
unknown <caroline.cunha.b@hotmail.com>
2022-01-24 06:19:59 -0300

    fim resolução challenge-06

--- challenge-06/index.js
+++ challenge-06/index.js
@@ -10,3 +10,27 @@ function showTeamPosition(position){
     }
     return 'O time que está em '+ position + ' lugar é o '+ teams[position -1]
 }
+
+function convertHex(color){
+    var hexa;
+    switch(color){
+        case 'red':
+            hexa = '#FF000'
+            break
+        case 'blue':
+            hexa = '#0000FF'
+            break
+        case 'green':
+            hexa = '#00FF00'
+            break
+        case 'black':
+            hexa = '#000000'
+            break
+        case 'white':
+            hexa = '#FFFFFF'
+            break
+        default:
+            return 'Não temos o equivalente hexadecimal para a cor ' + color
+    }
+    return 'O hexadecimal para a cor '+ color + ' é ' + hexa
+}
