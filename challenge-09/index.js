6f4ee863de04b36069c4993dcc15e6548640e725
unknown <caroline.cunha.b@hotmail.com>
2022-02-01 06:13:14 -0300

    fim resolução challenge-09

--- challenge-09/index.js
+++ challenge-09/index.js
@@ -14,3 +14,35 @@ function hoisting(){
     }
 }
 console.log(hoisting())
+
+function calculator(num1, num2){
+    return function(callback){
+        return callback(num1, num2)
+    }
+}
+
+var sum = calculator(10,2)
+console.log(sum(function(number1, number2){
+    return number1 + number2
+}))
+
+var substraction = calculator(5,2)
+var multiplication = calculator(3,3)
+var division = calculator(100,12)
+var mod = calculator(50,32)
+
+console.log(substraction(function(number1, number2){
+    return number1 - number2
+}))
+
+console.log(multiplication(function(number1,number2){
+    return number1 + number2
+}))
+
+ console.log(division(function(number1, number2){
+     return number1 / number2
+ }))
+
+ console.log(mod(function(number1, number2){
+     return number1 % number2
+ }))
