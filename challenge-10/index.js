b86a5bcc4646d2b92e3d55c994ab9060c3f0b5e8
unknown <caroline.cunha.b@hotmail.com>
2022-02-02 08:05:45 -0300

    challenge-10

---
+++ challenge-10/index.js
@@ -0,0 +1,13 @@
+var five = Number('5')
+console.log(five + ' é número?', typeof five === 'number')
+
+var concat = String(10) + 10
+console.log('"' + concat + '"' + ' é uma string? ', typeof concat === 'string')
+
+var operation = {
+    '+': function(x,y){return x + y},
+    '-': function(x,y){return x - y},
+    '*': function(x,y){return x * y},
+    '/': function(x,y){return x / y},
+    '%': function(x,y){return x % y}
+}
