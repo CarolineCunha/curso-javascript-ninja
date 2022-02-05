34b9ee638a7de4621aaaa9a39638fc15f10c16ef
unknown <caroline.cunha.b@hotmail.com>
2022-02-05 08:51:32 -0300

    challenge-11

---
+++ challenge-11/index.js
@@ -0,0 +1,25 @@
+(function(){
+
+    var once  = false
+    do {
+        console.log('Passou aqui')
+    } while(once)
+
+    var person = {
+        name: 'Carol',
+        age:'28',
+        weight: 48,
+        birthday: '18/06'
+    }
+
+    var counter = 0
+    for(var prop in person){
+        console.log('The ' + prop + ' of person is ' + person[prop])
+        counter ++
+    }
+    console.log('The person has ' + counter + ' properties')
+
+    function moreThan(age){
+       return person.age > age
+    }
+})()
