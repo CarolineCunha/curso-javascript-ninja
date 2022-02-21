37c67b6972837cc78b31171ccd284bfa7a05ace7
unknown <caroline.cunha.b@hotmail.com>
2022-02-21 07:57:07 -0300

    challenge-16

---
+++ challenge-16/index.js
@@ -0,0 +1,7 @@
+(function(){
+    'use strict'
+    var name = 'Carol';
+    for(var i = 0, len = name.length; i < len; i++){
+        console.log(name.charAt(i) + ' é a ' + (i + 1) + ' letra do meu nome  ');
+    }
+})()
