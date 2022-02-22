81b5249a77e4d6e0acc87c313f9b793028009a83
unknown <caroline.cunha.b@hotmail.com>
2022-02-22 08:33:59 -0300

    challenge-16

--- challenge-16/index.js
+++ challenge-16/index.js
@@ -4,4 +4,17 @@
     for(var i = 0, len = name.length; i < len; i++){
         console.log(name.charAt(i) + ' é a ' + (i + 1) + ' letra do meu nome  ');
     }
+    
+    var fullName = 'caroline-batista';
+    var newFullName = fullName.split('-').map((name)=> {
+        return name.charAt(0).toUpperCase() + name.slice(1);
+    }).join(' ');
+    console.log(newFullName);
+
+    var friends = ['Maria', 'Lucia', 'Ana', 'Camila', 'Carol']
+    var phrase = friends.reduce((acumulado, atual, index)=> {
+        var separator = friends.length - 1 === index ? ' e ' : ', ';
+        return acumulado + separator + atual;
+    }).concat(' são meus amigos')
+    console.log(phrase)
 })()
